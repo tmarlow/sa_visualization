4d66efe43a982ec46acbe0b154e40f0a39eab6c7
Thomas Marlow <thomas_marlow@brown.edu>
2020-04-03 16:10:58 -0400

    update for winter 2020 institue.

--- server.R
+++ server.R
@@ -2,19 +2,23 @@ library(shiny)
 library(ggplot2)
 library(RColorBrewer)
 library(plotly)
-
+library(spdep)
 
 n  <- 100 # number of obs to simulate
 
 # Set up a square lattice region
 simgrid <- expand.grid(x = 1:10, y = 1:10)
-X  <- rnorm(n, 0 , 10)
+X  <- rnorm(n, 0 , 2)
 
 ## This function simulates the data given a value of 
 ## spatial autocorrelation
 sa_var  <- function(rho_value, dat, lag){
   ##inverse weight matrix
-  w  <- nblag_cumul(nblag(cell2nb(10,10, type = "queen"), lag))
+  if(lag > 1){
+      w  <- nblag_cumul(nblag(cell2nb(10,10, type = "queen"), lag))
+  } else {
+    w <- cell2nb(10, 10, type = "queen")
+  }
   iw  <- invIrM(w, rho_value)
   result  <- iw %*% dat
   tibble(z = result)
@@ -25,11 +29,11 @@ shinyServer(function(input, output) {
   rv <- reactiveValues(data=X)
 
   observeEvent(input$goButton,{
-    rv$data <- rnorm(100, 0, 10)
+    rv$data <- rnorm(100, 0, 2)
   })
 
   formulaText <- reactive({
-    paste("SA = ", input$sa)
+    paste("Spatial Autocorrelation = ", input$sa)
   })
   
   output$caption <- renderText({
@@ -42,10 +46,33 @@ shinyServer(function(input, output) {
     sim_data <- bind_cols(simgrid, z)
     p <- ggplot(sim_data, aes(x = x, y = y, fill = z)) +
       geom_raster() +
-      scale_fill_viridis_c() +
+      #scale_fill_gradientn(colors = viridis_pal()(9), limits=c(-12, 12))
+      scale_fill_viridis_c(limits = c(-5, 5), oob = scales::squish) +
       theme_void() + 
-      theme(legend.position = "bottom")
+      labs(fill = "Value")
     ggplotly(p)
   })
+  
+  output$scatter <- renderPlotly({
+      X <- rv$data
+      z <- sa_var(input$sa, X, input$lag)
+      ## Create the spatial weights list from nb list
+      w <- cell2nb(10, 10, type = "queen")
+      listw <- nb2listw(w) 
+      sim_data <- bind_cols(simgrid, z)
+      sim_data$l <- lag.listw(x = listw, sim_data$z)
+      p2 <- ggplot(sim_data, aes(x = z, y = l, color = z)) +
+        geom_hline(yintercept = 0) + 
+        geom_vline(xintercept = 0) +
+        geom_point() +
+        scale_color_viridis_c(limits = c(-5, 5), 
+                              oob = scales::squish, guide = FALSE) + 
+        geom_smooth(method = 'lm', se = FALSE, color = "red") +
+        coord_equal() + 
+        theme_minimal() + 
+        labs(x = "Values", y = "Average Values of Neighboring Cells")
+      ggplotly(p2)
+  })
+  
 }
 )
--- ui.R
+++ ui.R
@@ -9,6 +9,14 @@
 
 library(shiny)
 library(shinythemes)
+library(tidyverse)
+library(viridis)
+library(ggplot2)
+library(RColorBrewer)
+library(plotly)
+library(spdep)
+
+
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
@@ -20,25 +28,55 @@ shinyUI(fluidPage(
   # Sidebar with a slider input for number of bins 
     sidebarPanel(
        sliderInput("sa",
-                   "Autocorrelation Coefficient:",
+                   "Spatial Autocorrelation Coefficient:",
                    min = -.9,
                    max = .9,
                    value = 0),
-       
+
+      p("   
+        Use the slider to adjust the spatial autocorrelation coefficient and observe how the data changes in the
+        figures to the right."),
+      p("
+        Spatial autocorrelation (SA) is the correlation of values in a single variable as a result of
+        geographic proximity. The result is neighbors sharing similar values. SA is often measured
+        as a correlation coefficient that ranges from -1 to +1. Negative SA is the non-random
+        separation of similar values (e.g. a checkerboard pattern). Positive SA results in the spatial clustering
+        of similar values."),
+
        br(),
        
     selectInput("lag",
-                "Order of Adjacency:",
-                c(1,2, 3, 4, 5)
+                "Order of Contiguity:",
+                c(1,2, 3, 4)
                 ),
+    p("
+        Change the order of contiguity and observe how the pattern of SA changes.
+      The ''order'' defines how many sets of neighbors are impacted by SA. 
+      First order includes only the immediate neighbors. Second order includes
+      your neighbors and your neighbors neighbors. And so on."),
     
+      p("
+        Spatial autocorrelation also has a distance across works.
+      This distance is highly context specific (social distance ≠ physical distance) and we must take care to think
+      hard about which types of distance matter and explore how our decisions impact the results. 
+      In many situations, contiguity of areal units is a reasonable approximation of the spatial structure of the data. 
+      "),
+      
+
+      
     br(), 
     
-    actionButton("goButton", "Re-Run the Model")),
+    actionButton("goButton", "Re-Simulate Data"),
+    p("Data is drawn from a normal distribution with a mean
+      of 0 and standard deviation of 2. As the autocorrelation increases
+      the raw data is rescaled to fit between -5, and 5.")
+    ),
     # Show a plot of the generated distribution
     mainPanel(
       h3(textOutput("caption")),
-      plotlyOutput("plot")
+      plotlyOutput("plot"),
+      br(),
+      plotlyOutput("scatter")
     )
   )
 ))
